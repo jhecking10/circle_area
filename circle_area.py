05357b0360cacbe0ae76c42d8629518b95e48bc4
jhecking10 <jhecking10@gmail.com>
2024-11-04 21:13:42 +0900

    initial commit

---
+++ circle_area.py
@@ -0,0 +1,32 @@
+from math import pi
+
+def area_of_circle(radius):
+    result = pi * radius ** 2
+    return round(result, 2)
+
+def display_radius():
+    while True:
+        try:
+            user_input = int(input("\nPlease type your circle's radius below.\n"))
+            print(f"\nThe area of this circle is {area_of_circle(user_input)}\n")
+            break
+        except:
+            user_input = TypeError
+            print("\nPlease type a numerical value.")
+
+def loop_program():
+    while True:
+        cont = input("Would you like to calculate the area of another circle? (Y/N):\n").strip().lower()
+        if cont == "y":
+            display_radius()
+        elif cont == "n":
+            print("\nThanks for using this program! Goodbye.")
+            break
+        else:
+            print('Plese type either "Y" or "N"')
+
+def main():
+    display_radius()
+    loop_program()
+
+main()
